3bc09847fbca67c0b54ddf67838f124743cfbf0c
glenpoin <develop.frei@gmail.com>
2024-06-09 16:59:59 +0300

    upd

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ OHMYPOSH_DIR="$HOME/.config/ohmyposh"
 
 if [ ! -d "$OHMYPOSH_DIR" ]; then
   mkdir -p "$OHMYPOSH_DIR"
-  cp ./ohmyposh/* "$OHMYPOSH_DIR"
+fi
+
+cp ./ohmyposh/* "$OHMYPOSH_DIR"
+
+
+if [ -f "$HOME/.zshrc" ]; then
+  mv $HOME/.zshrc $HOME/.zshrc.bak3
   cp ./.zshrc $HOME/
 fi
